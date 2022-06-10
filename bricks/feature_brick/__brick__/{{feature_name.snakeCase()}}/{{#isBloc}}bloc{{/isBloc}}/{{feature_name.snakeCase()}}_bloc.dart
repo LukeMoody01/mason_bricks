8b1a36bbee00c412e5bf5f4de4b4c243df10645c
Luke Moody <luke.moody@xam-consulting.com>
2022-06-10 14:04:01 +1000

    fix(feature_brick): Fix mising event import

--- bricks/feature_brick/__brick__/{{feature_name.snakeCase()}}/{{#isBloc}}bloc{{/isBloc}}/{{feature_name.snakeCase()}}_bloc.dart
+++ bricks/feature_brick/__brick__/{{feature_name.snakeCase()}}/{{#isBloc}}bloc{{/isBloc}}/{{feature_name.snakeCase()}}_bloc.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';{{#use_equatable}}
-import 'package:equatable/equatable.dart';
-part '{{feature_name.snakeCase()}}_event.dart';{{/use_equatable}}
+import 'package:equatable/equatable.dart';{{/use_equatable}}
+part '{{feature_name.snakeCase()}}_event.dart';
 part '{{feature_name.snakeCase()}}_state.dart';
 
 class {{feature_name.pascalCase()}}Bloc extends Bloc<{{feature_name.pascalCase()}}Event, {{feature_name.pascalCase()}}State> {
